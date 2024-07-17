f18346e29c96e61597aac9260b3720ac2aa4f96b
Quam22 <sdbolawale@gmail.com>
2024-07-17 20:06:08 +0845

    finalized project

---
+++ routes/jobRouter.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const auth = require("../middleware/auth");
+const {
+  getAllJobs,
+  getLatestJobs,
+  getSingleJob,
+  createJob,
+  applyForJob,
+  getUsersAppliedJobs,
+  updateJobStatus,
+  getUniqueLocations
+} = require("../controllers/jobController");
+
+router.route("/").get(getAllJobs).post(createJob);
+router.get('/locations', getUniqueLocations)
+router.get("/latest", getLatestJobs);
+router.post("/apply/:jobId", auth, applyForJob);
+router.get("/user", auth, getUsersAppliedJobs);
+router.route("/:jobId").get(getSingleJob).patch(auth, updateJobStatus);
+
+module.exports = router;
